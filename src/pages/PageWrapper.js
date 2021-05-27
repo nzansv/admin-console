8785b4e19cfa6a30de9cb3a208386fdf2e612731
nzansv <63594033+nzansv@users.noreply.github.com>
2021-05-27 17:14:07 +0600

    *

--- src/pages/PageWrapper.js
+++ src/pages/PageWrapper.js
@@ -1,11 +1,13 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageHeader from "./PageHeader";
+import Search from "../components/Search";
 
 export default function PageWrapper() {
     return (
        <div>
            <PageHeader/>
+           <Search/>
        </div>
 
     )
