5085ef0b67b52963c33b33020a714d907c17c415
nurdauletalim <78347524+nurdauletalim@users.noreply.github.com>
2021-05-27 18:40:21 +0600

    table

--- src/pages/PageWrapper.js
+++ src/pages/PageWrapper.js
@@ -2,12 +2,14 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageHeader from "./PageHeader";
 import Search from "../components/Search";
+import UserTable from "./userTable";
 
 export default function PageWrapper() {
     return (
        <div>
            <PageHeader/>
            <Search/>
+           <UserTable/>
        </div>
 
     )
--- src/pages/userTable.js
+++ src/pages/userTable.js
@@ -1,10 +1,24 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/userTable.css'
+import reload from '../assets/icons/reload.svg'
+import add from '../assets/icons/add.svg'
 
-function UserTable() {
+export default function UserTable() {
     return (
-        <h1>hello</h1>
+        <div className="userTable">
+            <div className="upperTableToDo">
+                <h5>Результаты поиска</h5>
+                <div className="toDoButton">
+                <a href="#" className="btn btn-primary"><img src={add} alt=""/> Создать</a>
+                <a href="#" className="btn" id="btn-update">Редактировать</a>
+                <a href="#" className="btn" id="btn-update">Удалить</a>
+                <a href="#" className="btn" id="btn-update">Сбросить пароль</a>
+                <a href=""><img src={reload} alt=""/></a>
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default UserTable;
+
